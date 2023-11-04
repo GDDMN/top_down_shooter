9c32d0c6ea95d889af518f1fbc4eaf4120d5516b
AlexTyazh <tyazhelnikov.98@mail.ru>
2023-11-05 02:22:59 +0300

    AmmoConfig

---
+++ Assets/Scripts/Configs/AmmoConfig.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "AmmoConfig", menuName ="Configs/Ammo")]
+public class AmmoConfig : ScriptableObject
+{
+    [SerializeField] private AmmoData _data;
+
+    public void Init()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Configs/AmmoData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public struct AmmoData
+{
+    public int _damage;
+    public int _dispersion;
+    public int _shootSpeed;
+}
