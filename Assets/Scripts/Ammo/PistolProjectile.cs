096ad7572136e97bc114b6f1fe25c9496c6b113e
GDDMN <apawkin@gmail.com>
2023-11-12 05:42:57 +0300

    commit

--- Assets/Scripts/Ammo/PistolProjectile.cs
+++ Assets/Scripts/Ammo/PistolProjectile.cs
@@ -1,13 +1,38 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 public class PistolProjectile : MonoBehaviour
 {
   [SerializeField] private Rigidbody2D _rigidbody;
   [SerializeField] private float _speed;
+  [SerializeField] private ParticleSystem _destroyParticle;
+
+  private float _time = 0f;
+  private readonly float _lifetimeSpeed = 3f; 
   public void Init(Transform initPoint)
   {
     _rigidbody.velocity = initPoint.up * (_speed * Time.deltaTime);
+    StartCoroutine(DyingTime());
+  }
+
+  private IEnumerator DyingTime()
+  {
+    while(_time <= 1f)
+    {
+      _time += _lifetimeSpeed * Time.deltaTime;
+      yield return null;
+    }
+
+    Die();
+  }
+
+  private void Die()
+  {
+    _destroyParticle.Play();
+    Destroy(this.gameObject);
+  }
+
+  private void OnCollisionEnter2D(Collision2D collision)
+  {
+    Die();
   }
 }
--- Assets/Scripts/Weapon/PistolWeapon.cs
+++ Assets/Scripts/Weapon/PistolWeapon.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 public class PistolWeapon : InlineFiredWeapon
 {
   [SerializeField] private PistolProjectile _projectile;
