4b44e961a5d58e70cdcb9ce97a4182bb457f97ce
GDDMN <apawkin@gmail.com>
2023-11-15 15:06:26 +0300

    add visual hurt/die effects

--- Assets/Scripts/Behaviours/ActorHurt.cs
+++ Assets/Scripts/Behaviours/ActorHurt.cs
@@ -3,35 +3,49 @@ using UnityEngine;
 public class ActorHurt : MonoBehaviour, IHurtable
 {
   [SerializeField] private int _hp = 4;
+  [Header("Visual")]
+  [SerializeField] private ParticleSystem _hitFX;
+  [SerializeField] private ParticleSystem _deathFX;
 
   public void HitProjectile(Projectile projectile)
   {
-    VisualImpact();
     GetDamage((int)projectile.Damage);
 
     if (_hp <= 0)
       Die();
-  }
 
+    VisualHitImpact();
+  }
   public void HitRay(int damage)
   {
-    VisualImpact();
     GetDamage(damage);
 
     if (_hp <= 0)
       Die();
+
+    VisualHitImpact();
   }
-  private void VisualImpact()
+
+  private void Die()
   {
-
+    VisualDieImpact();
+    Destroy(this.gameObject);
+  }
+  private void VisualHitImpact()
+  {
+    var hitFx = Instantiate(_hitFX, transform.position, Quaternion.identity);
+    hitFx.Play();
+    Destroy(hitFx.gameObject, 0.5f);
   }
 
+  private void VisualDieImpact()
+  {
+    var dieFx = Instantiate(_deathFX, transform.position, Quaternion.identity);
+    dieFx.Play();
+    Destroy(dieFx.gameObject, 2f);
+  }
   private void GetDamage(int damage)
   {
     _hp -= damage;
-  }
-  private void Die()
-  {
-    Destroy(this.gameObject);
   }
 }
