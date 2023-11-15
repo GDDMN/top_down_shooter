8fe3e12e0fa4ced60573888e1c7cb06c002297fb
GDDMN <apawkin@gmail.com>
2023-11-15 10:57:17 +0300

    add hurtable interface

--- Assets/Scripts/Interfaces/IHurtable.cs
+++ Assets/Scripts/Interfaces/IHurtable.cs
@@ -1,18 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
-public class IHurtable : MonoBehaviour
+public interface IHurtable
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  public void Hit(Collision2D collision);
 }
