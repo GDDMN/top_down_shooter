ba8f0642ee19c68e4350b900bce2a42c892d6305
GDDMN <apawkin@gmail.com>
2023-11-16 19:08:48 +0300

    update visual fx for ammo spawn point

--- Assets/Scripts/Enviroment/SpawnPoint.cs
+++ Assets/Scripts/Enviroment/SpawnPoint.cs
@@ -1,10 +1,18 @@
 using UnityEngine;
+using System.Collections;
 
 public class SpawnPoint : MonoBehaviour, IInteractable
 {
   [SerializeField] private AmmoType _ammoType;
+  [SerializeField] private float _timeToPickUp;
+  [Header("Visaul")]
   [SerializeField] private SpriteRenderer _icon;
+  [SerializeField] private Transform _image;
+  [SerializeField] private Animator _animator;
 
+  
+  [SerializeField, Range(0f, 1f)]private float _time = 0;
+  private bool _isActive = true;
   private void OnDrawGizmos()
   {
     _icon.sprite = G.Instance.Configs.Ammos.GetAmmoByEnum(_ammoType).Data.Icon;
@@ -15,7 +23,36 @@ public class SpawnPoint : MonoBehaviour, IInteractable
   }
   public void Interact(PlayerController player)
   {
+    if (_isActive == false)
+      return;
+
     player.PickUpWeapon(_ammoType);
+
+    Deactivate();
+    StartCoroutine(PickUpCooldown());
+  }
+  private IEnumerator PickUpCooldown()
+  {
+    while (_time < 1f)
+    {
+      _time += _timeToPickUp * Time.deltaTime;
+      yield return null;
+    }
+    Activate();
+  }
+
+  private void Activate()
+  {
+    _isActive = true;
+    _animator.SetTrigger("Release");
+    _time = 0;
+  }
+
+  private void Deactivate()
+  {
+    _isActive = false;
+    _animator.SetTrigger("PickedUp");
+    _time = 0;
   }
   private void OnTriggerEnter2D(Collider2D collision)
   {
